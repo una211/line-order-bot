822f6c654e26181a935b5e5ed48708046bbd7397
una211 <119406295+una211@users.noreply.github.com>
2026-04-02 11:31:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -635,5 +635,5 @@ app.post('/webhook', async (req, res) => {
 
 app.get('/', (req, res) => res.send('LINE 訂餐機器人運作中 🍔'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
